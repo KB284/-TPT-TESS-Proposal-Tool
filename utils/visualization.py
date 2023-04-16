c32c536bab42d55510aae94cf2aee51d55d86bb0
KB284 <66342672+KB284@users.noreply.github.com>
2023-04-16 12:58:59 -0400

    Revert "UI & functionality test"

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,7 +1,7 @@
 from bokeh.plotting import figure
 from bokeh.resources import Resources
 from bokeh.embed import file_html
-from flask import render_template, request
+from flask import render_template
 import numpy as np
 from bokeh.models import ColumnDataSource
 from astropy import units as u
